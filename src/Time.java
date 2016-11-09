124523a27e7e3814c5b1a93ffe0d6cc27ea81f41
Curtis Hohl <curtispatrick.hohl@calbaptist.edu>
2016-11-09 01:07:40 -0800

    init

---
+++ src/Time.java
@@ -0,0 +1,36 @@
+/**
+ * Created by curti_000 on 11/9/2016.
+ */
+public class Time {
+
+    public Time(int hour, int minute, boolean PM){
+
+    }
+
+    public Time fromString (String timeString) throws IllegalArgumentException {
+
+    }
+
+    public boolean equals (o) throws IllegalArgumentException{
+
+    }
+
+    public int getHour(){
+
+    }
+
+    public int getMinute(){
+
+    }
+
+    public boolean isPM(){
+
+    }
+
+    public void shift (int minutes){
+
+    }
+    public String toString(){
+
+    }
+}
---
+++ tst/TimeTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by curti_000 on 11/9/2016.
+ */
+public class TimeTest {
+}
