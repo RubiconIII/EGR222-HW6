7796b762cbbbe252ce07b65253d5e18f317c716f
Curtis Hohl <curtispatrick.hohl@calbaptist.edu>
2016-11-15 14:42:40 -0800

    push 10

--- src/Weekday.java
+++ src/Weekday.java
@@ -8,12 +8,13 @@ public enum Weekday {
     THURSDAY,
     FRIDAY;
 
-
+    //converts to first cap string with rest lowercase
     @Override
     public String toString() {
         String orgString = super.toString();
         return orgString.substring(0, 1) + orgString.substring(1).toLowerCase();
 
+    //converts to single letter short string
     }
     public String toShortName() {
         if (this == THURSDAY)
@@ -21,6 +22,7 @@ public enum Weekday {
 
             return super.toString().substring(0, 1);
         }
+    //reads string using enum
     public static Weekday fromString (String str){
         if (str == null || str.length() < 1)
             throw new IllegalArgumentException();
--- tst/WeekdayTest.java
+++ tst/WeekdayTest.java
@@ -9,8 +9,10 @@ public class WeekdayTest {
     public void toStringTest(){
         Weekday monday = Weekday.MONDAY;
         Weekday tuesday = Weekday.TUESDAY;
+        Weekday thurs = Weekday.THURSDAY;
         Assert.assertEquals("Monday", monday.toString());
         Assert.assertEquals(("Tuesday"), tuesday.toString());
+        Assert.assertEquals(("Thursday"), thurs.toString());
     }
 
     @Test
@@ -18,10 +20,12 @@ public class WeekdayTest {
         Weekday monday = Weekday.MONDAY;
         Weekday thurs = Weekday.THURSDAY;
         Weekday fri = Weekday.FRIDAY;
+        Weekday wednesday = Weekday.WEDNESDAY;
 
         Assert.assertEquals("M", monday.toShortName());
         Assert.assertEquals("R", thurs.toShortName());
         Assert.assertEquals("F", fri.toShortName());
+        Assert.assertEquals("W", wednesday.toShortName());
     }
 
     @Test
@@ -30,6 +34,8 @@ public class WeekdayTest {
         Assert.assertEquals(Weekday.MONDAY, mon);
         Weekday thur = Weekday.fromString("R");
         Assert.assertEquals(Weekday.THURSDAY, thur);
+        Weekday wednesday = Weekday.fromString("wedNESDAY");
+        Assert.assertEquals(Weekday.WEDNESDAY, wednesday);
 
     }
 }
