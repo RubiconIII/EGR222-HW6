e2c6f5e1cc7c14f9cbf0f3e0a9cbb2c0af6482bc
Curtis Hohl <curtispatrick.hohl@calbaptist.edu>
2016-11-29 23:30:28 -0800

    push 12

--- tst/CourseTest.java
+++ tst/CourseTest.java
@@ -9,139 +9,155 @@ import java.util.Set;
  * Created by curti_000 on 11/29/2016.
  */
 public class CourseTest {
-
-    static Set<Weekday> toEnumSet(String days){
-        Set<Weekday> set = null;
-        for(int i = 0 ; i < days.length(); i++){
-            if(set == null)
-                set = EnumSet.of(Weekday.fromString(days.charAt(i)+""));
-            else
-                set.add(Weekday.fromString(days.charAt(i)+""));
+        private void constructorNegativeHelper(String name, int credits, Set<Weekday> days, Time startTime, int duration, String error) {
+            try {
+                Course c = new Course(name, credits, days, startTime, duration);
+                Assert.fail(error);
+            } catch(IllegalArgumentException e) {
+            }
         }
-        return set;
-    }
 
-    private void conflictTestHelper(boolean expectedResult, String t1, String t2, String d1, String d2, int m1, int m2){
-        Set<Weekday> days1 = toEnumSet(d1);
-        Set<Weekday> days2 = toEnumSet(d2);
+        @Test
+        public void constructorTest() {
+            Time startTime = new Time(1, 15, true);
+            Set<Weekday> days = EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
+            Course phy201 = new Course("PHY 201", 4, days, startTime, 120);
+            Assert.assertEquals("PHY 201", phy201.getName());
+            Assert.assertEquals(4, phy201.getCredits());
+            Assert.assertTrue(startTime.equals(phy201.getStartTime()));
+            Assert.assertEquals(120, phy201.getDuration());
+        }
 
-        Course c1 = new Course("EGR 111", 3, days1, Time.fromString(t1), m1);
-        Course c2 = new Course("EGR 222", 3, days2, Time.fromString(t2), m2);
+        @Test
+        public void constNegativeTest() {
+            String name = "PHY 201";
+            Time startTime = new Time(1, 15, true);
+            Set<Weekday> days = EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
 
-        Assert.assertEquals("conflict should return " + expectedResult, expectedResult, c1.conflictsWith(c2));
-        Assert.assertEquals("conflict should return " + expectedResult, expectedResult, c2.conflictsWith(c1));
-    }
+            constructorNegativeHelper("", 4, days, startTime, 120, "Cannot have an empty course name!");
+            constructorNegativeHelper(null, 4, days, startTime, 120, "Cannot have a null course name!");
+            constructorNegativeHelper("PHY201", 4, days, startTime, 120, "Cannot have a space in the course name!");
+            constructorNegativeHelper(name, 0, days, startTime, 120, "Credits cannot be 0!");
+            constructorNegativeHelper(name, 10, days, startTime, 120, "Credits cannot exceed 5!");
+            constructorNegativeHelper(name, -7, days, startTime, 120, "Credits cannot be negative!");
+            constructorNegativeHelper(name, 4, null, startTime, 120, "Days cannot be null!");
+            constructorNegativeHelper(name, 4, days, null, 120, "StartTime cannot be null!");
+            constructorNegativeHelper(name, 4, days, startTime, 0, "Duration cannot be 0!");
+        }
 
-    private void containsTestHelper(boolean expectedResult, String t1, String d1, int m1, Weekday d, Time t){
-        Set<Weekday> days1 = toEnumSet(d1);
-        Course c1 = new Course("EGR 111", 3, days1, Time.fromString(t1), m1);
-        String message = "should";
-        if(!expectedResult)
-            message = "should not";
-        Assert.assertEquals("Course " + c1 + " " + message + " contain " + t, expectedResult, c1.contains(d, t));
-    }
+        @Test
+        public void startTimeTest() {
+            Time startTime = new Time(1, 15, true);
+            Set<Weekday> days = EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
+            Course phy201 = new Course("PHY 201", 4, days, startTime, 120);
+            Assert.assertTrue(Time.fromString("01:15 PM").equals(phy201.getStartTime()));
+            startTime.shift(60);
+            Assert.assertTrue(Time.fromString("02:15 PM").equals(startTime));
+            Assert.assertFalse(startTime.equals(phy201.getStartTime()));
+            Assert.assertTrue(Time.fromString("01:15 PM").equals(phy201.getStartTime()));
+        }
 
-    private void endTimeTestHelper(String startTimeStr, String expectedEndTimeStr, int duration){
-        Time startTime = Time.fromString(startTimeStr);
-        Time expectedEndTime = Time.fromString(expectedEndTimeStr);
-        Set<Weekday> days =  EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
-        Course c = new Course("EGR 111", 3, days, startTime, duration);
-        Assert.assertTrue("End time should be " + expectedEndTimeStr + " when start time is " + startTimeStr +
-                " and duration is " + duration, expectedEndTime.equals(c.getEndTime()));
-    }
+        @Test
+        public void endTimeTest() {
+            Set<Weekday> d = new HashSet<Weekday>();
+            d.add(Weekday.MONDAY);
+            d.add(Weekday.WEDNESDAY);
+            d.add(Weekday.FRIDAY);
 
-    private void constNegativeHelper(String n, int c, Set<Weekday> d, Time s, int m, String message){
-        try {
-            Course t = new Course(n, c, d, s, m);
-            Assert.fail(message);
-        }catch(IllegalArgumentException e){
+            Time t = new Time(01, 15, true);
+            Course c = new Course("PHY 203", 4, d, t, 120);
+
+            Time test1 = new Time(03, 15, true);
+            Assert.assertEquals(test1, c.getEndTime());
+
+            Time t2 = new Time(01, 15, false);
+            Course c2 = new Course("PHY 203", 4, d, t2, 120);
+
+            Time test2 = new Time(03, 15, false);
+            Assert.assertEquals(test2, c2.getEndTime());
+        }
+
+        @Test
+        public void equalsTest() {
+            Time t1 = new Time(12, 30, true);
+            Time t2 = new Time(12, 30, false);
+
+            Set<Weekday> d = new HashSet<>();
+            d.add(Weekday.MONDAY);
+            d.add(Weekday.WEDNESDAY);
+            d.add(Weekday.FRIDAY);
+
+            Course c1 = new Course("EGR 182", 3, d, t1, 50);
+            Assert.assertTrue(c1.equals(c1));
+
+            Course c2 = new Course("EGR 222", 3, d, t2, 50);
+            Assert.assertFalse(c1.equals(c2));
+
+            Course c3 = new Course("EGR 225", 3, d, t1, 50);
+            Assert.assertFalse(c1.equals(c3));
+
+            Course c4 = new Course("EGR 182", 3, d, t1, 50);
+            Assert.assertTrue(c1.equals(c4));
+
+            Course c5 = new Course("EGR 101", 2, d, t1, 30);
+            Assert.assertFalse(c1.equals(c5));
+        }
+
+        @Test
+        public void containsTest() {
+            Time t1 = new Time(12, 30, true);
+            Time t2 = new Time(12, 30, false);
+            Time t3 = new Time(10, 30, true);
+            Time t4 = new Time(12, 40, true);
+            Time t5 = new Time(11, 19, true);
+
+            Set<Weekday> d = new HashSet<>();
+            d.add(Weekday.MONDAY);
+            d.add(Weekday.WEDNESDAY);
+            d.add(Weekday.FRIDAY);
+
+            Course c1 = new Course("EGR 225", 3, d, t1, 50);
+
+            Assert.assertTrue(c1.contains(Weekday.MONDAY, t1));
+            Assert.assertTrue(c1.contains(Weekday.WEDNESDAY, t1));
+            Assert.assertTrue(c1.contains(Weekday.FRIDAY, t1));
+            Assert.assertFalse(c1.contains(Weekday.TUESDAY, t1));
+            Assert.assertFalse(c1.contains(Weekday.THURSDAY, t1));
+            Assert.assertFalse(c1.contains(Weekday.MONDAY, t3));
+            Assert.assertTrue(c1.contains(Weekday.MONDAY, t4));
+
+            Course c2 = new Course("ICS 431", 3, d, t3, 50);
+
+            Assert.assertFalse(c2.contains(Weekday.MONDAY, t2));
+            Assert.assertTrue(c2.contains(Weekday.MONDAY, t3));
+            Assert.assertTrue(c2.contains(Weekday.MONDAY, t5));
+        }
+
+        @Test
+        public void conflictsWithTest() {
+            Time t1 = new Time(8, 15, false);
+            Time t2 = new Time(9, 15, false);
+
+            Set<Weekday> d1 = EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
+            Set<Weekday> d2 = EnumSet.of(Weekday.TUESDAY, Weekday.THURSDAY);
+
+            Course c1 = new Course("EGR 182", 4, d1, t1, 60);
+            Assert.assertTrue(c1.conflictsWith(c1));
+            Course c2 = new Course("EGR 182", 4, d2, t2, 90);
+            Assert.assertFalse(c1.conflictsWith(c2));
+        }
+
+        @Test
+        public void hashCodeTest() {
+            Time startTime = new Time(1, 15, true);
+            Set<Weekday> days = EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
+            Course courseOne = new Course("PHY 201", 4, days, startTime, 120);
+            Course courseTwo = new Course("PHY 201", 4, days, startTime, 120);
+
+            Set<Course> courses = new HashSet<>();
+            courses.add(courseOne);
+            Assert.assertTrue(courses.contains(courseOne));
+            Assert.assertTrue(courses.contains(courseTwo));
+            Assert.assertEquals(courseOne.hashCode(), courseTwo.hashCode());
         }
     }
-
-    @Test
-    public void constructorTest(){
-        Time startTime = new Time (5, 0, true);
-        Set<Weekday> days =  EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
-        Course egr222 = new Course("EGR 222", 3, days, startTime, 60);
-        Assert.assertEquals("EGR 222", egr222.getName());
-        Assert.assertEquals(3, egr222.getCredits());
-        Assert.assertTrue(startTime.equals(egr222.getStartTime()));
-        Assert.assertEquals(60, egr222.getDuration());
-        //Assert.assertTrue(days.equals(egr222.getDaysOffered()));
-    }
-
-    @Test
-    public void constructorNegativeTest(){
-        String name = "EGR 222";
-        Time startTime = new Time (5, 0, true);
-        Set<Weekday> days =  EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
-
-        //invalid arguments
-        constNegativeHelper("", 3, days, startTime, 60, "Empty name should be invalid");
-        constNegativeHelper(null, 3, days, startTime, 60, "null name should be invalid");
-        constNegativeHelper("EGR222", 3, days, startTime, 60, "Name should contain space");
-        constNegativeHelper(name, 0, days, startTime, 60, "0 credit should be invalid");
-        constNegativeHelper(name, 6, days, startTime, 60, "6 credit or above should be invalid");
-        constNegativeHelper(name, -20, days, startTime, 60, "negative credit should be invalid");
-        constNegativeHelper(name, 3, null, startTime, 60, "null days should be invalid");
-        constNegativeHelper(name, 3, days, null, 60, "null time should be invalid");
-        constNegativeHelper(name, 3, days, startTime, 0, "duration should be greater than 0");
-    }
-
-    @Test
-    public void endTimeTest(){
-        endTimeTestHelper("11:59 AM", "12:00 PM", 1);
-        endTimeTestHelper("11:00 AM", "12:00 PM", 60);
-        endTimeTestHelper("11:59 AM", "12:00 PM", 1);
-        endTimeTestHelper("11:00 AM", "01:00 PM", 120);
-        endTimeTestHelper("10:00 AM", "12:05 PM", 125);
-        endTimeTestHelper("07:30 AM", "10:00 AM", 150);
-        endTimeTestHelper("11:00 PM", "11:59 PM", 59);
-    }
-
-    @Test
-    public void startTimeTest(){
-        Time startTime = new Time (5, 0, true);
-        Set<Weekday> days =  EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
-        Course egr222 = new Course("EGR 222", 3, days, startTime, 60);
-        Assert.assertTrue(Time.fromString("05:00 PM").equals(egr222.getStartTime()));
-        startTime.shift(90);
-        Assert.assertTrue(Time.fromString("06:30 PM").equals(startTime));
-        Assert.assertFalse(startTime.equals(egr222.getStartTime()));
-        Assert.assertTrue(Time.fromString("05:00 PM").equals(egr222.getStartTime()));
-    }
-
-    @Test
-    public void conflictTest(){
-        conflictTestHelper(false, "05:00 PM", "04:59 PM", "MW", "W", 1, 1);
-        conflictTestHelper(true, "05:00 PM", "04:59 PM", "MWF", "W", 1, 2);
-        conflictTestHelper(false, "05:00 PM", "04:59 PM", "MWF", "R", 1, 2);
-        conflictTestHelper(false, "03:00 AM", "02:30 AM", "M", "M", 60 , 30);
-        conflictTestHelper(false, "12:00 PM", "11:30 AM", "F", "F", 60, 15);
-        conflictTestHelper(true, "11:00 AM", "12:30 PM", "T", "T", 120, 1);
-        conflictTestHelper(false, "11:00 AM", "12:30 PM", "T", "R", 120, 1);
-    }
-
-    @Test
-    public void containsTest(){
-        containsTestHelper(true, "10:00 AM", "MWF", 1, Weekday.FRIDAY, new Time(10, 0, false));
-        containsTestHelper(false, "10:00 AM", "MWF", 1, Weekday.TUESDAY, new Time(10, 0, false));
-        containsTestHelper(false, "10:00 AM", "MWF", 1, Weekday.FRIDAY, new Time(10, 1, false));
-        containsTestHelper(true, "10:00 AM", "T", 120, Weekday.TUESDAY, new Time(11,59, false));
-        containsTestHelper(false, "10:00 AM", "T", 120, Weekday.TUESDAY, new Time(12,0, true));
-    }
-
-    @Test
-    public void hashCodeTest(){
-        Time startTime = new Time (5, 0, true);
-        Set<Weekday> days =  EnumSet.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY);
-        Course c1 = new Course("EGR 222", 3, days, startTime, 60);
-        Course c2 = new Course("EGR 222", 3, days, startTime, 60);
-
-        Set<Course> courses = new HashSet<>();
-        courses.add(c1); //added c1
-        Assert.assertTrue(courses.contains(c1));
-        Assert.assertTrue(courses.contains(c2)); //should return true for c2 as well
-        Assert.assertEquals(c1.hashCode(), c2.hashCode());
-    }
-}
